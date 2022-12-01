a0f8f47f31c57f49e9223bc06edbf307cc57076d
David3H <henrychimborazo331@gmail.com>
2022-12-01 18:15:40 -0500

    commit 18:15

--- app/src/main/java/com/example/reivanrickyprueba/MainActivity.java
+++ app/src/main/java/com/example/reivanrickyprueba/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.reivanrickyprueba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private void Bloquear(EditText editText){
+        editText.setText(null);
+        editText.setEnabled(false);
+        editText.setCursorVisible(false);
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
