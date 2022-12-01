93d9249ebf0e2f92a1cda9e7b4031d52e8567cb9
David3H <henrychimborazo331@gmail.com>
2022-12-01 17:45:34 -0500

    commit 17:45

---
+++ app/src/main/java/com/example/reivanrickyprueba/Tres.java
@@ -0,0 +1,14 @@
+package com.example.reivanrickyprueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tres);
+    }
+}
